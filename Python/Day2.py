965199513141a366a820dbb86942084f44c33c3b
itsrohitcodes <rohitkumar33222@gmail.com>
2026-08-05 11:41:18 +0530

    Day 2 changes

---
+++ Python/Day2.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = int(input())
+
+total = a + b
+difference = a - b
+product = a * b
+division = a / b
+floor_division = a // b
+modulus = a % b
+
+print("The sum of", a, "and", b, "is", total)
+print("The difference of", a, "and", b, "is", difference)
+print("The product of", a, "and", b, "is", product)
+print("The division of", a, "and", b, "is", division)
+print("The floor division of", a, "and", b, "is", floor_division)
+print("The modulus of", a, "and", b, "is", modulus)   
