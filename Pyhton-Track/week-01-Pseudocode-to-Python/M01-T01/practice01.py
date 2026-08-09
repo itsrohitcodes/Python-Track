65a54e81498379687127af6c098ed02c12852d96
itsrohitcodes <rohitkumar33222@gmail.com>
2026-08-09 12:38:31 +0530

    Pseudocode to Python code

---
+++ Pyhton-Track/week-01-Pseudocode-to-Python/M01-T01/practice01.py
@@ -0,0 +1,11 @@
+# Convert Pseudocode to Python code
+
+# Declare variables
+first_number = 12
+second_number = 8
+
+# Calculate the sum of the two numbers
+total = first_number + second_number
+
+# Display the sum of the two numbers
+print(total)
