14c62de79f4edd30c4a4478f2ce81afc9a2ac830
itsrohitcodes <rohitkumar33222@gmail.com>
2026-08-11 20:03:04 +0530

    Create and call a function

---
+++ Pyhton-Track/week-01-Pseudocode-to-Python/M01-T04/practice01.py
@@ -0,0 +1,9 @@
+# Create and call display_welcome() function
+
+# Create the display welcome() function below
+def display_welcome():
+    name = input()
+    print("Welcome, " + name + "!")
+
+# Call the function
+display_welcome()
