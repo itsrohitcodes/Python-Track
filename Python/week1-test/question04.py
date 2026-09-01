eee3f842dd77f1160cf616c1a0b5728ca9eabaf8
itsrohitcodes <rohitkumar33222@gmail.com>
2026-09-01 08:33:31 +0530

    Completed Question 4

--- Python/week1-test/question04.py
+++ Python/week1-test/question04.py
@@ -1,3 +1,6 @@
+# QUESTION 04
+
+# Function to calculate total amount, discount, and final amount
 def calculate_bill(unit_price, quantity):
     total_amount = unit_price * quantity
 
@@ -10,14 +13,14 @@ def calculate_bill(unit_price, quantity):
 
     return total_amount, discount, final_amount
 
-
+# Take input from the user
 product_name = input()
 unit_price = int(input())
 quantity = int(input())
 
 total_amount, discount, final_amount = calculate_bill(unit_price, quantity)
 
-
+# Print the results
 print(f"Product Name: {product_name}")
 print(f"Price: {unit_price:.2f}")
 print(f"Quantity: {quantity}")
